b22e7e618e5bd17fda32ae5f4fdcc5bdcbce2fc7
Darya Kokorina <darya.kakoryna@instinctools.com>
2019-11-13 10:33:10 +0300

    finish project

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 import * as actions from "../actions";
 
 const Counter = ({ counter, inc, dec, rnd }) => {
@@ -26,8 +25,4 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators(actions, dispatch);
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, bindActionCreators } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./components/app";
 
 import reducer from "./reducer";
-import * as actions from "./actions";
 
 const store = createStore(reducer);
-const { dispatch } = store;
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
 ReactDOM.render(
   <Provider store={store}>
